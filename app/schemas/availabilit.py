3b7f86108e89a82146e8d3889939e8259901a43c
João <galhardojoaopedro@gmail.com>
2024-10-09 20:29:42 -0300

    Add Availability model to store user availability information

---
+++ app/schemas/availabilit.py
@@ -0,0 +1,13 @@
+
+
+from pydantic import BaseModel
+
+from app.enum.time_slot import TimeSlot
+
+
+class Availability(BaseModel):
+    availability_id: str
+    user_id: str
+    day_of_week: str
+    time_slot: TimeSlot
+    status: bool
