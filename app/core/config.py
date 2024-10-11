d01ee06afc0433bacbc57a7508a33df5c6952f98
João <galhardojoaopedro@gmail.com>
2024-10-11 00:54:42 -0300

    Refactor config.py and update database URL

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,8 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
+
 
 class Settings(BaseSettings):
-    CLIENT_ID: str
-    CLIENT_SECRET: str
-    REDIRECT_URI: str = "http://localhost:8000/auth/callback"
-    AUTH_URL: str = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
-    TOKEN_URL: str = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
-    SCOPE: str = "openid profile email"
-    DATABASE_URL: str = "postgresql://postgres:postgres@db/postgres"
+    DATABASE_URL: str = "postgresql://fontys:fontys@db/fontys"
 
     class Config:
         env_file = ".env"
