95834c9bacd0628411e126eaa5dc97db0b6ef3ea
João <galhardojoaopedro@gmail.com>
2024-10-11 00:53:37 -0300

    Add logging configuration and root endpoint

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+
+import logging
 from fastapi import FastAPI
 from app.api.v1 import user
+
+# Configurando o logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 app = FastAPI()
 
 app.include_router(user.router, prefix="/api/v1/users", tags=["users"])
+
+@app.get("/")
+async def read_root():
+    logger.info("Acessando a raiz")
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
