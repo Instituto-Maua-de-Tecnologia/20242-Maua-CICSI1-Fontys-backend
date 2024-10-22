a6dd38fb0a11b45547508df939e41badb95e232e
João <galhardojoaopedro@gmail.com>
2024-10-22 15:32:07 -0300

    Refactor user_repository.py: Add create_user endpoint

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,25 +1,19 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
-from app.schemas.user import UserCreate
+from app.schemas.user import User
 
 class UserRepository:
     def __init__(self, db: Session):
         self.db = db
-        
-    @staticmethod
-    async def create_or_update_user(user_info: dict, db: Session):
-        user = db.query(User).filter(User.microsoft_id == user_info["id"]).first()
-        if not user:
-            user = User(
-                microsoft_id=user_info["id"],
-                email=user_info["userPrincipalName"],
-                name=user_info["displayName"]
-            )
-            db.add(user)
-        else:
-            user.name = user_info["displayName"]
-            user.email = user_info["userPrincipalName"]
-        
-        db.commit()
-        return user
-    
+
+    def create_user(self, user: User):
+        new_user = User(
+            user_id=user.user_id,
+            email=user.email,
+            name=user.name,
+            password=user.password,  # Aqui você deve garantir que o password seja criptografado
+        )
+        self.db.add(new_user)
+        self.db.commit()
+        self.db.refresh(new_user)
+        return new_user
