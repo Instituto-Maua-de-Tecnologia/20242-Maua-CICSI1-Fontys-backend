2c9996710b797d46efba2aa7fd368e697d8ed715
João <galhardojoaopedro@gmail.com>
2024-10-22 15:31:13 -0300

    Refactor config.py: Update DATABASE_URL and add DATABASE_URL_AIVEN

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,11 @@
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://fontys:fontys@db/fontys"
+    DATABASE_URL: str
+    DATABASE_URL_AIVEN: str
 
     class Config:
-        env_file = ".env"
+        env_file = ".env"  # Carrega variáveis do arquivo .env
 
+# Instância global de configurações
 settings = Settings()
