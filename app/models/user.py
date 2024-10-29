16214de6fdac38c99583dec1495a3112381715e7
João <galhardojoaopedro@gmail.com>
2024-10-29 17:35:26 -0300

    Refactor User model to include microsoft_id and use enums for status and user types

--- app/models/user.py
+++ app/models/user.py
@@ -2,16 +2,20 @@ from sqlalchemy import Column, Integer, String, Enum
 from app.db.base import Base
 from sqlalchemy.orm import relationship
 
+from app.enum.status_type import StatusType
+from app.enum.user_type import UserType
+
 
 class User(Base):
     __tablename__ = "users"
 
-    user_id = Column(String(200), primary_key=True, index=True)
-    email = Column(String(255), unique=True, nullable=False)
-    password = Column(String(255), nullable=False)
-    status = Column(Enum("Active", "Inactive"), default="Active")
-    name = Column(String(255), nullable=False)
-    user_type = Column(Enum("Secretary", "Professor", "Coordinator"), nullable=False)
+    user_id = Column(String(), primary_key=True, index=True)
+    microsoft_id = Column(String(), nullable=False)
+    email = Column(String(), unique=True, nullable=False)
+    password = Column(String(), nullable=False)
+    status = Column(Enum(StatusType), default="Active", nullable=False)
+    name = Column(String(), nullable=False)
+    user_type = Column(Enum(UserType), nullable=False)
     notes = Column(String(500), nullable=True)
 
     subjects = relationship("UserSubject", back_populates="user")
