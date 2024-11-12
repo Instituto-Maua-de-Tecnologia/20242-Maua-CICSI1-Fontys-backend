f69f74ead339b07a0016727b88056aaa610a8c46
João <galhardojoaopedro@gmail.com>
2024-11-12 18:04:18 -0300

    changes user table to 'user' and removes unnecessary attributes

--- app/models/user.py
+++ app/models/user.py
@@ -1,22 +1,16 @@
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, String, Enum
 from app.db.base import Base
 from sqlalchemy.orm import relationship
 
-from app.enum.status_type import StatusType
 from app.enum.user_type import UserType
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     user_id = Column(String(), primary_key=True, index=True)
     microsoft_id = Column(String(), nullable=False)
-    email = Column(String(), unique=True, nullable=False)
-    password = Column(String(), nullable=False)
-    status = Column(Enum(StatusType), default=StatusType.VERIFIED, nullable=False)
     name = Column(String(), nullable=False)
     user_type = Column(Enum(UserType), nullable=False)
     notes = Column(String(500), nullable=True)
 
-    subjects = relationship("UserSubject", back_populates="user")
-    availabilities = relationship("Availability", back_populates="user")
