8af1f015f0da89c01ed78c9c3e2b3bc3a0e406a0
João <galhardojoaopedro@gmail.com>
2024-11-13 12:00:12 -0300

    fix: update tablename from "type" to "types" in Type model

--- app/models/type.py
+++ app/models/type.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, String
 
 
 class Type:
-    __tablename__ = "type"
+    __tablename__ = "types"
     
     type_id = Column(String, primary_key=True, index=True)
     type_name = Column(String, nullable=False)
