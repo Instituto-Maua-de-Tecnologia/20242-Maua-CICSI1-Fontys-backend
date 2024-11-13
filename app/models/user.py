8d26e0df58c2d08457e55e030fd52ea0064e5f1c
João <galhardojoaopedro@gmail.com>
2024-11-13 12:02:35 -0300

    fix: update tablename from "user" to "users" and modify import path for Base in User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,16 +1,13 @@
-from sqlalchemy import Column, String, Enum
-from app.db.base import Base
-from sqlalchemy.orm import relationship
-
-from app.enum.user_type import UserType
-
+from sqlalchemy import Column, String
+from app.core.database import Base, engine
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     user_id = Column(String(), primary_key=True, index=True)
     microsoft_id = Column(String(), nullable=False)
     name = Column(String(), nullable=False)
-    user_type = Column(Enum(UserType), nullable=False)
     notes = Column(String(500), nullable=True)
 
+if __name__ == '__main__':
+    Base.metadata.create_all(bind=engine)
