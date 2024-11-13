18da8a99ff3d5660132bd6b2d05aafb7f3ee6e6e
João <galhardojoaopedro@gmail.com>
2024-11-13 12:00:01 -0300

    fix: update import path for Base in Slots model and modify schema

--- app/models/slots.py
+++ app/models/slots.py
@@ -1,10 +1,13 @@
-from app.db.base import Base
-from sqlalchemy import Column, String
+from app.core.database import Base
+from sqlalchemy import Column, Enum, String, Time
+
+from app.enum.days_of_week import DayOfWeek
 
 
 class Slots(Base): 
-    __tablename__ = "slot"
+    __tablename__ = "slots"
     
     slot_id = Column(String, primary_key=True, index=True)
-    day_time = Column(String, nullable=False)
+    day_of_week = Column(Enum(DayOfWeek), nullable=False)
+    time = Column(Time, nullable=False)
     
