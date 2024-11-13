525c4e019ba34e3215ef35d7dcc49c6f00b600a7
João <galhardojoaopedro@gmail.com>
2024-11-13 12:02:29 -0300

    fix: update import path for Base in UserType model and change tablename to "user_types"

--- app/models/user_type.py
+++ app/models/user_type.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, ForeignKey, String
-from app.db.base import Base
+from app.core.database import Base
 from sqlalchemy.orm import relationship
 
 class UserType(Base):
-    __tablename__ = "user_type"
+    __tablename__ = "user_types"
     
+    user_type_id = Column(String, primary_key=True, index=True)
     user_id = Column(String, ForeignKey("users.user_id"), nullable=False)
     type_id = Column(String, ForeignKey("types.type_id"), nullable=False)
     
