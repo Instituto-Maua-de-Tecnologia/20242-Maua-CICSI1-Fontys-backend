55334f14dff7f0a950acc21313ff08d82db3a6a4
João <galhardojoaopedro@gmail.com>
2024-11-13 11:58:40 -0300

    remove: delete database session and base files

--- app/db/base.py
+++
@@ -1,7 +0,0 @@
-from app.db.session import engine
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-def create_tables():
-    Base.metadata.create_all(bind=engine)
