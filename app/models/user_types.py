a83170ea6a024d85a6702c54b4b7a79991f0c698
João <galhardojoaopedro@gmail.com>
2024-11-13 23:14:18 -0300

    feat: rename user_type.py to user_types.py and update relationships in UserType model

--- app/models/user_types.py
+++ app/models/user_types.py
@@ -1,14 +1,15 @@
 from sqlalchemy import Column, ForeignKey, String
-from app.core.database import Base
 from sqlalchemy.orm import relationship
+from app.core.database import Base, engine
 
 class UserType(Base):
     __tablename__ = "user_types"
     
     user_type_id = Column(String, primary_key=True, index=True)
     user_id = Column(String, ForeignKey("users.user_id"), nullable=False)
-    type_id = Column(String, ForeignKey("types.type_id"), nullable=False)
+    type_id = Column(String, ForeignKey("type_users.type_id"), nullable=False)
     
-    
-    users = relationship("User", back_populates="types")
-    types = relationship("Type", back_populates="users")
+    user = relationship("User", back_populates="user_type")
+    type_user = relationship("TypeUser", back_populates="user_type")
+
+
