661831e0b94511de2c81b3a2cc7b4acc8a47f892
João <galhardojoaopedro@gmail.com>
2024-11-13 23:41:10 -0300

    feat: remove UserEntity model from the codebase

--- app/domain/entities/user.py
+++
@@ -1,9 +0,0 @@
-
-from typing import Optional
-from pydantic import BaseModel
-
-class UserEntity(BaseModel):
-    user_id: str
-    microsoft_id: Optional[str] = None
-    name: str
-    notes: Optional[str] = True
