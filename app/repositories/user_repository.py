2b274d8572d789b6c672f39400e53340618ec94c
João <galhardojoaopedro@gmail.com>
2024-11-13 23:41:25 -0300

    refactor: update user_repository to use user_id for retrieval and include photo and notes fields

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,35 +1,35 @@
 from sqlalchemy.orm import Session
 from app.models.users import User  # Modelo do banco de dados
 from app.domain.interfaces.repositories.user_repository_interface import IUserRepository
-from app.domain.entities.user import UserEntity  # Entidade de domínio
+from app.domain.entities.user_entity import UserEntity  # Entidade de domínio
 
 class UserRepository(IUserRepository):
     def __init__(self, db: Session):
         self.db = db
 
-    def get_user_by_email(self, email: str) -> UserEntity:
-        db_user = self.db.query(User).filter(User.email == email).first()
+    def get_user_by_id(self, user_id: str) -> UserEntity:
+        db_user = self.db.query(User).filter(User.user_id == user_id).first()
         if db_user:
             return UserEntity(
-                id=db_user.id,
+                id=db_user.user_id,
                 name=db_user.name,
-                email=db_user.email,
-                password=db_user.hashed_password
+                photo=db_user.photo,
+                notes=db_user.notes
             )
         return None
 
     def create_user(self, user: UserEntity) -> UserEntity:
         db_user = User(
             name=user.name,
-            email=user.email,
-            hashed_password=user.password
+            photo=user.photo,
+            notes=user.notes
         )
         self.db.add(db_user)
         self.db.commit()
         self.db.refresh(db_user)
         return UserEntity(
-            id=db_user.id,
+            id=db_user.user_id,
             name=db_user.name,
-            email=db_user.email,
-            password=db_user.hashed_password
+            photo=db_user.photo,
+            notes=db_user.notes
         )
