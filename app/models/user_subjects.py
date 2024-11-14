c3ceac362732097a4c4672cbb0a50ea69ee2f91a
João <galhardojoaopedro@gmail.com>
2024-11-13 23:14:10 -0300

    feat: add UserSubject model with relationships to User and Subject

---
+++ app/models/user_subjects.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+from app.core.database import Base, engine
+
+class UserSubject(Base):
+    __tablename__ = "user_subjects"
+    
+    user_subject_id = Column(String, primary_key=True, index=True)
+    user_id = Column(String, ForeignKey("users.user_id"), nullable=False)
+    subject_code = Column(String, ForeignKey("subjects.subject_code"), nullable=False)
+
+    user = relationship("User", back_populates="user_subject")
+    subject = relationship("Subject", back_populates="user_subject")
+
+
