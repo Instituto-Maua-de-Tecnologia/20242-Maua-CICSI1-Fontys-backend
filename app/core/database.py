73ccfb600f9b615ccbc1b867f1275d2970fbb262
João <galhardojoaopedro@gmail.com>
2024-11-26 21:45:46 -0300

    feat: add get_db function to manage database sessions

--- app/core/database.py
+++ app/core/database.py
@@ -3,8 +3,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
+
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL_AIVEN
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+
+def get_db():
+    db = SessionLocal()  # Cria uma nova sessão
+    try:
+        yield db  # Fornece a sessão para uso
+    finally:
+        db.close() 
