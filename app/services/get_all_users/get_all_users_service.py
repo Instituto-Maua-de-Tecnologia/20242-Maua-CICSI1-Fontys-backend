4074e0bf61e3205880250f578d5f4cf0307089ae
João <galhardojoaopedro@gmail.com>
2024-11-27 18:33:06 -0300

    refactor: modify GetAllUsersService to accept a database session in the constructor

--- app/services/get_all_users/get_all_users_service.py
+++ app/services/get_all_users/get_all_users_service.py
@@ -1,9 +1,9 @@
 from app.repositories.user_repository import UserRepository
-
+from sqlalchemy.orm import Session
 
 class GetAllUsersService:
-    def __init__(self):
-        self.user_repository = UserRepository()
+    def __init__(self, db: Session):
+        self.user_repository = UserRepository(db)
 
     def execute(self):
         return self.user_repository.get_all_users()
