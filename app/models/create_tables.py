c3d818673094c8302ca360903234593a0789cc03
YuriDrapack <drapackyuri@gmail.com>
2024-11-27 20:33:07 -0300

    refactor: remove unnecessary comment in create_tables.py

--- app/models/create_tables.py
+++ app/models/create_tables.py
@@ -1,6 +1,6 @@
 from app.core.database import Base, engine
 
 if __name__ == "__main__":
-    # Criar todas as tabelas no banco de dados
+    
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
