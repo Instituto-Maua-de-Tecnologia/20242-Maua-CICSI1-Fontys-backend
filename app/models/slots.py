d7ab67dfdc8eb3ed14e555de3366464479604ab3
João <galhardojoaopedro@gmail.com>
2024-11-28 00:31:27 -0300

    refactor: update Slot model to use DayOfWeekEnum instead of DayOfWeek

--- app/models/slots.py
+++ app/models/slots.py
@@ -1,7 +1,7 @@
 from app.core.database import Base
 from sqlalchemy import Column, DateTime, Enum as SqlEnum, Integer
 
-from app.enums.days_of_week import DayOfWeek
+from app.enums.days_of_week_enum import DayOfWeekEnum
 from sqlalchemy.orm import relationship
 
 
@@ -9,7 +9,7 @@ class Slot(Base):
     __tablename__ = "slots"
     
     slot_id = Column(Integer, primary_key=True, index=True)
-    day_of_week = Column(SqlEnum(DayOfWeek), nullable=False)
+    day_of_week = Column(SqlEnum(DayOfWeekEnum), nullable=False)
     time = Column(DateTime, nullable=False)
     
     availability = relationship("Availability", back_populates="slot")
