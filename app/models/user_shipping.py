94bbcde3bc938f957011223293a4260b216f95d2
João <galhardojoaopedro@gmail.com>
2024-11-28 00:31:36 -0300

    refactor: update UserShipping model to use DateTime for shipping_date and StatusTypeEnum for status

--- app/models/user_shipping.py
+++ app/models/user_shipping.py
@@ -1,8 +1,8 @@
 
 from app.core.database import Base
-from sqlalchemy import Column, Date, Enum as SqlEnum, ForeignKey, String
+from sqlalchemy import Column, DateTime, Enum as SqlEnum, ForeignKey, String
 from sqlalchemy.orm import relationship
-from app.enums.status_type import StatusType
+from app.enums.status_type_enum import StatusTypeEnum
 
 
 
@@ -11,8 +11,8 @@ class UserShipping(Base):
 
     shipping_id = Column(String, primary_key=True, index=True)
     user_id = Column(String, ForeignKey('users.user_id'), nullable=False)
-    shipping_date = Column(Date, nullable=False)
-    status = Column(SqlEnum(StatusType), nullable=False)
+    shipping_date = Column(DateTime, nullable=False)
+    status = Column(SqlEnum(StatusTypeEnum), nullable=False)
     
     user = relationship("User", back_populates="user_shipping")
     
