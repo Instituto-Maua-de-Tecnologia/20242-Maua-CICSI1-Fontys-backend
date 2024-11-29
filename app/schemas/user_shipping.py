aaee95993ef8dcbc83cc35b9132e531bf07ebe62
YuriDrapack <drapackyuri@gmail.com>
2024-11-28 23:08:23 -0300

    refactor: update status field to use StatusTypeEnum

--- app/schemas/user_shipping.py
+++ app/schemas/user_shipping.py
@@ -2,10 +2,10 @@
 from pydantic import BaseModel
 from datetime import date
 
-from app.enums.status_type_enum import StatusType
+from app.enums.status_type_enum import StatusTypeEnum
 
 class UserShippingBase(BaseModel):
     shipping_id: str
     user_id: str
     shipping_date: date
-    status: StatusType
+    status: StatusTypeEnum
