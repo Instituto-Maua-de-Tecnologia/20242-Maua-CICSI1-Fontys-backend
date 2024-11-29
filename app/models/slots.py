d8104a4e30d126704613736a075ad1fad7b7ff09
YuriDrapack <drapackyuri@gmail.com>
2024-11-28 23:03:42 -0300

    refactor: change time column type to SqlEnum using TimeSlotEnum

--- app/models/slots.py
+++ app/models/slots.py
@@ -4,13 +4,15 @@ from sqlalchemy import Column, DateTime, Enum as SqlEnum, Integer
 from app.enums.days_of_week_enum import DayOfWeekEnum
 from sqlalchemy.orm import relationship
 
+from app.enums.time_slot_enum import TimeSlotEnum
+
 
 class Slot(Base): 
     __tablename__ = "slots"
     
     slot_id = Column(Integer, primary_key=True, index=True)
     day_of_week = Column(SqlEnum(DayOfWeekEnum), nullable=False)
-    time = Column(DateTime, nullable=False)
+    time = Column(SqlEnum(TimeSlotEnum), nullable=False)
     
     availability = relationship("Availability", back_populates="slot")
     schedule = relationship("Schedule", back_populates="slot")
