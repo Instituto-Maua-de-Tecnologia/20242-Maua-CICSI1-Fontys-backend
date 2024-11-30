14f5d893fd373f46737d62e2fef31a9d4f2ee58a
senn59 <74777070+senn59@users.noreply.github.com>
2024-11-30 19:26:13 +0100

    feat: add controller methods

--- app/api/v1/routes/availability.py
+++ app/api/v1/routes/availability.py
@@ -1,11 +1,13 @@
-from dbm import error
 from typing import List
+from warnings import deprecated
+
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
+
+from app.controllers.availability_controller.get_availability_controller import GetAvailabilityController
+from app.controllers.availability_controller.set_availability_controller import SetAvailabilityController
 from app.core.database import SessionLocal
-from app.domain.entities.availability_entity import AvailabilityEntity
-from app.schemas.availability import AvailabilityBase, GetUserAvailabilityRequest, SetAvailabilityRequest, \
-    AvailabilityResponseSchema
+from app.schemas.availability import  SetAvailabilityRequest, AvailabilityResponseSchema
 from app.services.availability_service import AvailabilityService
 
 router = APIRouter()
@@ -17,31 +19,26 @@ def get_db():
     finally:
         db.close()
 
-@router.get("/availability/", response_model=List[AvailabilityResponseSchema])
-def get_availability(user_id: str = Query(...), db: Session = Depends(get_db)):
+
+def get_availability_controller(db: Session = Depends(get_db)) -> GetAvailabilityController:
     service = AvailabilityService(db)
-    availability = service.get_user_availability(user_id)
+    return GetAvailabilityController(service)
 
-    response_objects = []
-    for a in availability:
-        response_objects.append(AvailabilityResponseSchema(
-            slot_id=a.slot.slot_id,
-            value=a.value,
-        ))
+def set_availability_controller(db: Session = Depends(get_db)) -> SetAvailabilityController:
+    service = AvailabilityService(db)
+    return SetAvailabilityController(service)
 
-    return response_objects
+@router.get("/availability/", response_model=List[AvailabilityResponseSchema])
+def get_availability(
+        user_id: str = Query(...),
+        controller: GetAvailabilityController = Depends(get_availability_controller)
+) -> List[AvailabilityResponseSchema]:
+    return controller.handle(user_id)
 
 @router.post("/availability/", response_model=List[AvailabilityResponseSchema])
-def set_availability(data: SetAvailabilityRequest, db: Session = Depends(get_db)):
-    service = AvailabilityService(db)
-    availability = service.set_availability(data.availabilities, data.user_id)
-
-    response_objects = []
-    for a in availability:
-        response_objects.append(AvailabilityResponseSchema(
-            slot_id=a.slot.slot_id,
-            value=a.value,
-        ))
-
-    return response_objects
+def set_availability(
+        data: SetAvailabilityRequest,
+        controller: SetAvailabilityController = Depends(set_availability_controller)
+) -> List[AvailabilityResponseSchema]:
+    return controller.handle(data)
 
