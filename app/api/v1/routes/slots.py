050d1d697ae15a55223d60d16b07c5363979ece9
João <galhardojoaopedro@gmail.com>
2024-12-01 20:02:48 -0300

    refactor: rename GetSlotsController to GetAllSlotsController and update service integration

--- app/api/v1/routes/slots.py
+++ app/api/v1/routes/slots.py
@@ -3,10 +3,10 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from app.controllers.slot_controller.get_slots_controller import GetSlotsController
+from app.controllers.slot_controller.get_all_slots_controller import GetAllSlotsController
 from app.core.database import SessionLocal
 from app.schemas.slots import SlotBase
-from app.services.slot_service import SlotService
+from app.services.slot_services.get_all_slots_service import GetAllSlotsService
 
 router = APIRouter()
 
@@ -18,12 +18,12 @@ def get_db():
         db.close()
 
 
-def get_slots_controller(db: Session = Depends(get_db)) -> GetSlotsController:
-    service = SlotService(db)
-    return GetSlotsController(service)
+def get_slots_controller(db: Session = Depends(get_db)) -> GetAllSlotsController:
+    service = GetAllSlotsService(db)
+    return GetAllSlotsController(service)
 
 @router.get("/slots/", response_model=List[SlotBase])
 def get_slots(
-        controller: GetSlotsController = Depends(get_slots_controller)
+        controller: GetAllSlotsController = Depends(get_slots_controller)
 ) -> List[SlotBase]:
     return controller.handle()
