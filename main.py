ec28a33a4c8f972740ed71438e4c41fcec914ab6
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2024-12-01 20:22:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -36,5 +36,6 @@ async def read_root():
     return {"message": "API is running successfully!"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
