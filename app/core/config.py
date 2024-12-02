294381417d6dfb8e653d8b65877f2c03e7a6480d
YuriDrapack <drapackyuri@gmail.com>
2024-12-01 23:23:14 -0300

    feat: add PORT configuration to Settings class in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -6,11 +6,13 @@ load_dotenv()
 class Settings(BaseSettings):
     DATABASE_URL_AIVEN: str
     API_KEY: str
+    PORT: int = 8000
 
     class Config:
         env_file = ".env"
 
 settings = Settings(
     DATABASE_URL_AIVEN=os.environ.get('DATABASE_URL_AIVEN'),
-    API_KEY=os.environ.get('API_KEY')
+    API_KEY=os.environ.get('API_KEY'),
+    PORT=os.environ.get('PORT')
 )
