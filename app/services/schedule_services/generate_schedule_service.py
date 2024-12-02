d4e4a931093071aa7cab921e7a18b3a97aad0388
João <galhardojoaopedro@gmail.com>
2024-12-02 06:14:26 -0300

    feat: rename semester_id to semester_number in GenerateScheduleSchema and update generate_schedule_service to accept semester_number

--- app/services/schedule_services/generate_schedule_service.py
+++ app/services/schedule_services/generate_schedule_service.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import Session
 
-from app.repositories.schedule_repository import ScheduleRepository
+from app.repositories.availability_repository import ScheduleRepository
 
 
 class GenerateScheduleService:
-    def __init__ (self, db: Session):
+    def _init (self, db: Session):
         self.schedule_repository = ScheduleRepository(db)
 
-    def execute(self):
-        response = self.schedule_repository.generate_schedule()
+    def execute(self, semester_number: int):
+        response = self.schedule_repository.generate_schedule(semester_number=semester_number)
         return response
