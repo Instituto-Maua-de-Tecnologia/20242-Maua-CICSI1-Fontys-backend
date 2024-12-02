2fb6f42f670782a7a2a1b8a135dd482bce84c198
João <galhardojoaopedro@gmail.com>
2024-12-01 23:23:15 -0300

    feat: add create schedule endpoint and controller integration

--- app/api/v1/routes/schedule.py
+++ app/api/v1/routes/schedule.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.controllers.schedule_controller.create_schedule_IA_controller import CreateScheduleIAController
+from app.controllers.schedule_controller.create_schedule_controller import CreateScheduleController
 from app.core.database import get_db
 from app.schemas.schedule import ScheduleResponseSchema
 from app.services.schedule_services.create_schedule_IA_service import CreateScheduleIAService
+from app.services.schedule_services.create_schedule_service import CreateScheduleService
 
 
 router = APIRouter()
@@ -13,10 +15,21 @@ def get_create_schedule_IA_controller(db: Session = Depends(get_db)) -> CreateSc
     service = CreateScheduleIAService(db)
     return CreateScheduleIAController(service)
 
+def get_create_schedule_controller(db: Session = Depends(get_db)) -> CreateScheduleController:
+    service = CreateScheduleService(db)
+    return CreateScheduleController(service)
+
 
 @router.post("/schedule_IA", response_model=ScheduleResponseSchema ,status_code=201)
 def create_schedule_IA(
     data: ScheduleResponseSchema,
     controller: CreateScheduleIAController = Depends(get_create_schedule_IA_controller)
+) -> ScheduleResponseSchema:
+    return controller.handle(data)
+
+@router.post("/schedule", response_model=ScheduleResponseSchema ,status_code=201)
+def create_schedule(
+    data: ScheduleResponseSchema,
+    controller: CreateScheduleController = Depends(get_create_schedule_controller)
 ) -> ScheduleResponseSchema:
     return controller.handle(data)
