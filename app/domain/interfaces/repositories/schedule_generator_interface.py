c9d85adcd5015a7922d0fe31e08fd25dfcb878f3
João <galhardojoaopedro@gmail.com>
2024-12-02 07:51:17 -0300

    feat: remove IScheduleGenerator interface as part of the schedule refactor

--- app/domain/interfaces/repositories/schedule_generator_interface.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from app.schemas.week import Week
-from app.domain.entities.schedule_entity import ScheduleEntity
-
-class IScheduleGenerator(ABC):
-    @abstractmethod
-    def order_schedule(self, list_available_teacher_subject_times, list_of_subjects, semester) -> Week:
-        pass
